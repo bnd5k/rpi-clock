759cb9874c9086bc317de758f4aa8156e47ea367
Ben Downey <Ben@BuildingBlockLabs.com>
2020-04-21 13:55:46 -0700

    Removing Traffic light Apparently, the traffic light isn't as cool as I had hoped.

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,7 @@
     <Clock />
 
     <div class="row second-row">
-
-        <TrafficLight ref="trafficLight" v-if="trafficLightVisible"/>
-
-        <Weather ref="weather" v-if="weatherVisible" />
-
+      <Weather ref="weather" />
     </div>
 
   </div>
@@ -16,57 +12,14 @@
 
 <script>
 import Clock from './components/Clock.vue'
-import TrafficLight from './components/TrafficLight.vue'
 import Weather from './components/Weather.vue'
 
 export default {
   name: 'app',
   components: {
     Clock,
-    TrafficLight,
     Weather
   },
-  data () {
-    return {
-      trafficLightVisible: false,
-      weatherVisible: false,
-    }
-  },
-  mounted () {
-    this.init();
-  },
-  methods: {
-    init () {
-      this.manageTrafficAndWeather();
-      setInterval(this.manageTrafficAndWeather, 2 * this.$minutes);
-    },
-    manageTrafficAndWeather () {
-      // Traffic light should be on from 7:00 pm - 7:00 am
-
-       var now = new Date();
-       var hour = now.getHours();
-
-       if (hour < 7 || hour > 17) {
-         this.ensureTrafficLightOn();
-         this.ensureWeatherOff();
-       } else {
-         this.ensureTrafficLightOff();
-         this.ensureWeatherOn();
-       }
-    },
-    ensureTrafficLightOn () {
-      this.trafficLightVisible = true;
-    },
-    ensureTrafficLightOff () {
-      this.trafficLightVisible = false;
-    },
-    ensureWeatherOn () {
-      this.weatherVisible = true;
-    },
-    ensureWeatherOff () {
-      this.weatherVisible = false;
-    }
-  }
 }
 </script>
 
